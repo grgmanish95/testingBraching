9139015895b5d4608323a8e9cc6f2ceb65300b62
Manish Gurung <mail2manishgrg@gmail.com>
2020-07-07 22:06:51 +0545

    testing1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
+mongoose.connect('mongodb:127.0.0.1:27017/branchingTesting', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true}) 
+    .then (() => {
+        console.log('Connected to database server');
+    });
+
+    app.get('/', (req, res) => {
+   res.send("welcome to my app");
+});
+
+
+
+app.listen(process.env.Port, ()=> {
+    console.log('Server is running at localhost:3000');
+});
